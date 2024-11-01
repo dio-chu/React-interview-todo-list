c0b8d4dc1b507b643b12f0ada043e706df4dabaf
dio-chu <dio.chu@104.com.tw>
2024-11-01 15:31:14 +0800

    feat:組件開發-button,checkout

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,7 +1,33 @@
+// hook
+import { useState } from "react";
+
+// component
+import DButton from "../components/DButton";
+import DCheckbox from "../components/DCheckbox";
+import { FaPlus } from "react-icons/fa";
+
 const TodoPage = () => {
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (checked) => {
+    setIsChecked(checked);
+    console.log("Checkbox value changed:", checked);
+  };
   return (
     <div>
       <h1>todopage</h1>
+
+      <DButton label="新增任務" density="comfortable" onClick={handleClick} />
+      <DButton label="新增任務" onClick={handleClick} />
+      <DButton label="新增" appendIcon={<FaPlus />} size="large" />
+      <DCheckbox
+        isChecked={isChecked}
+        onChange={handleCheckboxChange}
+        label="基本選項"
+      />
     </div>
   );
 };
