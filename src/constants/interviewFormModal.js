b5d9e25ed2891c5a9bba8b8687e971ce26fe181e
dio-chu <dio.chu@104.com.tw>
2024-11-04 16:56:48 +0800

    enum: 列舉處理

---
+++ src/constants/interviewFormModal.js
@@ -0,0 +1,9 @@
+export const INTERVIEW_MODAL_MODE = {
+  CREATE: "create",
+  EDIT: "edit",
+};
+
+export const INTERVIEW_MODAL_TITLE = {
+  [INTERVIEW_MODAL_MODE.CREATE]: "新增面試",
+  [INTERVIEW_MODAL_MODE.EDIT]: "編輯面試",
+};
