566ab4dab596f120a4d285fcfc516443361a3dd3
dio-chu <dio.chu@104.com.tw>
2024-11-04 11:06:51 +0800

    頁面切版 (#3)
    * feat:切版，調整scss
    
    * feat:add TodoPage layout
    
    ---------
    
    Co-authored-by: dio-chu <dio-chu@104.com.tw>

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,15 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TodoPage from "./pages/TodoPage";
 import ChartPage from "./pages/ChartPage";
 
+import "./styles/global.scss";
+
 function App() {
   return (
     <Router>
-      <div className="container mx-auto px-4">
-        <nav className="mb-4 p-4 border-b">
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/" className="hover:text-blue-500">
-                Todo List
-              </Link>
-            </li>
-            <li>
-              <Link to="/chart" className="hover:text-blue-500">
-                Chart
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<TodoPage />} />
-          <Route path="/chart" element={<ChartPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+        <Route path="/chart" element={<ChartPage />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/constants/interviewStatus.js
@@ -0,0 +1,23 @@
+export const INTERVIEW_STATUS = {
+  ALL: "all",
+  SCHEDULED: "scheduled",
+  COMPLETED: "completed",
+};
+
+export const INTERVIEW_STATUS_OPTIONS = [
+  {
+    id: "1",
+    value: INTERVIEW_STATUS.ALL,
+    label: "全部",
+  },
+  {
+    id: "2",
+    value: INTERVIEW_STATUS.SCHEDULED,
+    label: "已安排面試",
+  },
+  {
+    id: "3",
+    value: INTERVIEW_STATUS.COMPLETED,
+    label: "已有結果",
+  },
+];
